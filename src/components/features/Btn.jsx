4643134a440e376bdf52e8b927c7912353870e49
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-10-14 23:08:19 +0900

    custom button

--- src/components/features/Btn.jsx
+++ src/components/features/Btn.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const btn = () => {
-  return <div>btn</div>;
+const btn = (props) => {
+  /* label : button 이름, onClick : onClick, isDisabled : disabled, design : className */
+  const { label, feature, design, isDisable } = props;
+  return (
+    <button className={design} onClick={onClick} disabled={isDisable}>
+      {label}
+    </button>
+  );
 };
 
 export default btn;
